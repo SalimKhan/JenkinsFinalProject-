6583d2f36ac7feff432b1dee6e8a04a3864dacb0
student <salim_khan_152@yahoo.com>
2021-05-01 04:59:42 +0000

    This is final project for Jenkins

---
+++ src/main/java/com/JenkinsFinalProject/JenkinsFinalProject/JenkinsFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.JenkinsFinalProject.JenkinsFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JenkinsFinalProject/JenkinsFinalProject/JenkinsFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JenkinsFinalProject.JenkinsFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
